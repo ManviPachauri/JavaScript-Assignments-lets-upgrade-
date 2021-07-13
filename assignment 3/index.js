05d106393233647366185f7d7b031d7988005506
ManviPachauri <51271896+ManviPachauri@users.noreply.github.com>
2021-07-13 13:13:16 +0530

    assignment 3 submitted

---
+++ assignment 3/index.js
@@ -0,0 +1,8 @@
+// Day-4 Javascript Essential Program
+
+function on_click() {
+    let a = document.getElementById('one');
+    let b = a.value;
+
+    document.getElementById('input').innerText="Hello " +b;
+}
